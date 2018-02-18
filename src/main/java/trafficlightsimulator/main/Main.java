61b2be379f106aaa251a930302e50c65475d49da
Alexander Baumann <baumannalexj@gmail.com>
2018-02-17 21:59:32 -0600

    use maven structure, removed class files

---
+++ src/main/java/trafficlightsimulator/main/Main.java
@@ -0,0 +1,41 @@
+package trafficlightsimulator.main;
+
+import trafficlightsimulator.ui.*;
+//import myproject.model.Model;
+import trafficlightsimulator.model.swing.SwingAnimatorBuilder;
+
+/**
+ * A static class to demonstrate the visualization aspect of
+ * simulation.
+ */
+public class Main {
+	private Main() {}
+	public static void main(String[] args) {
+
+		
+		{
+			// WITH UI
+		UI ui = null;
+//		ui = new TextUI();
+		ui = new PopupUI();
+		Control control = new Control(new SwingAnimatorBuilder(), ui); //TODO takes interface animatorbuilder, could get textanimator working too
+		control.run();
+		}
+		
+		
+		{
+			// MANUAL
+//			int numRows = 3;
+//			int numCols = 3;
+//			Model m = new Model(new SwingAnimatorBuilder(), numRows,numCols); //2, 3);
+//			m.run(500); // TODO get test UI to work
+//			m.run(500);
+//			
+//			
+//			m.run(500);
+//			m.dispose();
+		}
+		System.exit(0);
+	}
+}
+
---
+++ src/main/java/trafficlightsimulator/ui/UIFormTest.java
@@ -0,0 +1,5 @@
+package trafficlightsimulator.ui;
+
+public interface UIFormTest {
+	public boolean run(String input);
+}
